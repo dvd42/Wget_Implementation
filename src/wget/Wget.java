bb4f26fabb3c6242be5e9e87278e2d50946141a4
DESKTOP-539MGN5\Diego <diegovd0296@gmail.com>
2017-02-24 23:05:07 +0100

    Wget(Session1) -missing tutoring

--- src/wget/Wget.java
+++ src/wget/Wget.java
@@ -2,9 +2,12 @@ package wget;
 
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,19 +20,30 @@ public class Wget {
 		
 	}
 
-	public static void readFile(String args) {
+	public static void readFile(String path) {
+		
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(args));
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line = br.readLine();
+		
+			//Create File output Stream to write the URL data to Web_data.txt
+			File file = new File("Web_data.txt");
 			
-			String s = br.readLine();
+			if (!file.exists()) {
+				file.createNewFile();
+			}
 			
-			while(s != null){
-				System.out.println(s);
-				saveURL(s);
-				s = br.readLine();
+			FileOutputStream fop = new FileOutputStream(file);
+			
+			//Read the whole file
+			while(line != null){
+				saveURL(line, fop);
+				line = br.readLine();
 			}
 			br.close();
-			
+			fop.flush();
+			fop.close();
+		
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -38,17 +52,31 @@ public class Wget {
 	}
 
 	
-	public static void saveURL(String s){
+	public static void saveURL(String line, FileOutputStream fop){
 		
 		try {
-			URL url = new URL(s);
 			
-			System.out.println(url.getFile());
+			InputStream is = null;
+			URL url = new URL(line);
+		
+			//open connection to URL to get data
+			is = url.openStream();
+
+			// get the content in bytes
+			byte[] contentInBytes = is.toString().getBytes();
 			
+			System.out.println(is.toString());
+			
+			fop.write(contentInBytes);
+			fop.write("\n".getBytes());
 			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-		} 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		
 		
 	}
 }
