3327473f7eb04289add1ef7d9d279f2af3a87b3f
diego <diego@diego-HP-ProBook-6460b>
2017-02-27 15:22:24 +0100

    Wget-Session1(pending tutoring)

--- src/wget/Wget.java
+++ src/wget/Wget.java
@@ -4,8 +4,8 @@ package wget;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -54,19 +54,18 @@ public class Wget {
             if (!f.exists()){
             	f.createNewFile();	
             }
-            
-            FileOutputStream fop = new FileOutputStream(f);
-            
+           
+            FileWriter fop = new FileWriter(f);
+            String s = bf.readLine();
             
             //Write the content of the web to the file 
-            while((line = bf.readLine()) != null){
+            while(s != null){
             	
-                byte[] contentInBytes = bf.readLine().getBytes();
-				fop.write(contentInBytes);
+                fop.write(s);
+                s = bf.readLine(); 
             }
             
             bf.close();
-            fop.flush(); //force out all the data from the stream
             fop.close();
 	        	
 	            
